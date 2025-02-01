fe7321b8ae39cfd42be3ebb7d05c72528d8498cc
yjhouma <yjhouma@users.noreply.github.com>
2025-02-01 09:17:00 +0700

    Update file 2/1/2025, 9:16:59 AM

--- 738-MonotoneIncreasingDigits/738-MonotoneIncreasingDigits.py
+++ 738-MonotoneIncreasingDigits/738-MonotoneIncreasingDigits.py
@@ -1,26 +1,12 @@
-class Solution:
-    def round_down_to_nearest_10(self,num, n):
-        pw = 10**n
-        return (num//pw) * pw
-    
-    def is_monotone_increasing(self,n):
-        while n>1:
-            curr_digit = n%10
-            n = n//10
-            prev_digit = n%10
-            if prev_digit > curr_digit:
-                return False
-        
-        return True
+"""
+Idea is simple, from index end to 0,
+if you find a position i where [i-1] > [i], then [i-1] -= 1 and from i, fill 9
+for example:
+    123 41 789
+position i is 5, [4] > [5] ==> 4 > 1, so 123 39 999
 
-    def monotoneIncreasingDigits(self, n: int) -> int:
-
-        pw = 1
-        while not self.is_monotone_increasing(n):
-            n = self.round_down_to_nearest_10(n, pw) - 1
-            pw += 1
-
-        
-        return n
-
-        
+for example:
+    332
+i = 2, 329
+i = 1, 299
+"""
